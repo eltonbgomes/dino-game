c50236069df3868bfceff3dd994dc6ef328417f2
Elton Barbosa Gomes <eltonbarbosagomes@gmail.com>
2021-01-30 02:50:59 -0300

    fazendo o cactus se mover

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 //console.log(dino);
 let isJumping = false;
 
-function handleKeyUp(event){ //funcao para funcionar apenas com o espaco pressionado
+function handleKeyDown(event){ //funcao para funcionar apenas com o espaco pressionado
     if(event.keyCode === 32){
         console.log("Pressionou espaço");
         if(!isJumping){
@@ -36,4 +37,31 @@ function jump(){ //funcao que fará o dino pular
     }, 20);
 }
 
-document.addEventListener('keyup', handleKeyUp); // identifica qualquer botao pressionado
+function createCactus(){
+    const cactus = document.createElement('div'); //gerar html para insercao na pagina index
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 4500;
+
+    console.log(randomTime);
+
+    cactus.classList.add('cactus'); //adiciona uma classe para 'cactus' para poder modificar com css
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus); //adiciona o cactus no background
+
+    //movimentando o cactus
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -70){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else{
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime); //cria um novo cactus com um tempo aleatorio
+}
+
+createCactus();
+
+document.addEventListener('keydown', handleKeyDown); // identifica qualquer botao pressionado
